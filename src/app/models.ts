7b0520f62395ad1a05ca4d722b3c950acbf66544
AlanKowalzky <alan.kowalzky@gmail.com>
2025-08-04 11:27:51 +0200

    init: smart-ui part2 b: angular-part2

--- src/app/models.ts
+++ src/app/models.ts
@@ -21,10 +21,28 @@ export interface Device extends BaseItem {
 
 export type CardItem = Sensor | Device;
 
+// API Models
+export interface DeviceItem {
+  type: 'device';
+  icon: string;
+  label: string;
+  state: boolean;
+}
+
+export interface SensorItem {
+  type: 'sensor';
+  icon: string;
+  label: string;
+  value: {
+    amount: number;
+    unit: string;
+  };
+}
+
 export interface Card {
   id: string;
   title: string;
-  layout: 'horizontalLayout' | 'verticalLayout' | 'singleDevice';
+  layout: 'singleDevice' | 'horizontalLayout' | 'verticalLayout';
   items: CardItem[];
 }
 
@@ -32,4 +50,28 @@ export interface Tab {
   id: string;
   title: string;
   cards: Card[];
+}
+
+export interface Dashboard {
+  id: string;
+  title: string;
+  icon: string;
+}
+
+export interface DashboardData {
+  tabs: Tab[];
+}
+
+export interface UserProfile {
+  fullName: string;
+  initials: string;
+}
+
+export interface LoginRequest {
+  userName: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
 }
